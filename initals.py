b44866e654c1af10be339cda6210be431109cec5
specks-dining <133817000+specks-dining@users.noreply.github.com>
2023-05-16 14:06:05 -0700

    Create initals.py

---
+++ initals.py
@@ -0,0 +1,14 @@
+# My name is Specks Dining.
+# I enjoy sci-fi.
+
+name = '''
+ SSS   DDDD 
+S   S  D    D
+S      D    D
+ SSS   D    D
+    S  D    D
+S   S  D    D
+ SSS   DDDD 
+'''
+
+print(name)
